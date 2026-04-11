91dd641ce87fbc02c9fac92440bf1b5fd72b8c18
Hugo <tyqo@Mac-mini-de-Hugo.local>
2026-04-11 18:31:50 +0200

    ijijiij

--- app/stock/page.tsx
+++ app/stock/page.tsx
@@ -8,7 +8,7 @@ import CartDrawer from '@/components/CartDrawer';
 
 const THEME = { 
   category: 'Signaletique', 
-  label: 'Signalétique & Stock', 
+  label: 'Produits non personnalisés', 
   color: 'text-green-500', 
   bg: 'bg-green-500' 
 };
